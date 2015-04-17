04947a1c325b099d897e599e56f3e1ef3f89cd7f
Vinh Bui <bicyclethief@gmail.com>
2015-04-17 16:17:09 -0400

    Setup associations between Choice and User

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,6 @@
 class Choice < ActiveRecord::Base
   belongs_to :question
+
+  has_many :choice_users
+  has_many :users, through: :choice_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :survey_users
   has_many :surveys_taken, through: :survey_users, class_name: "Survey", source: :user
 
+  has_many :choice_users
+  has_many :choices, through: :choice_users, class_name: "Choice", source: :choice
+
   has_many :surveys_created, class_name: "Survey", foreign_key: :user_id
 
   validates :name, uniqueness: true
