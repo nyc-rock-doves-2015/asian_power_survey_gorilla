c46a9ddae525d9f9cdb7a1dee1d72f773db088eb
Vinh Bui <bicyclethief@gmail.com>
2015-04-17 16:19:03 -0400

    Seed db with data as if all surveys were taken

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@ require 'faker'
 
 User.create(name: "John", password: "123");
 
+# create random users
 10.times do
   user = User.create(name: Faker::Name.name, password: "123")
 end
 
+# create random surveys with questions and choices
 5.times do
   survey = Survey.create(title: Faker::Commerce.department, user_id: User.pluck(:id).sample)
 
@@ -13,5 +15,13 @@ end
 
   7.times do
     Choice.create(description: Faker::Lorem.word, question_id: question.id)
+  end
+end
+
+# have random users take random surveys
+Survey.all.each do |survey|
+  survey.questions.each do |question|
+    choice_id = question.choices.pluck(:id).sample
+    ChoiceUser.create(choice_id: choice_id, user_id: User.pluck(:id).sample)
   end
 end
