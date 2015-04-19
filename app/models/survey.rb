06efa81eaeb4aa5d27c58883b30099217cdb0647
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-19 14:48:39 -0400

    fixed bug with survey table associations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 class Survey < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: :user_id
   has_many :survey_users
-  has_many :takers, through: :survey_users, class_name: "User", source: :survey
+  has_many :takers, through: :survey_users, class_name: "User", source: :user
   has_many :questions
 
   mount_uploader :image, ImageUploader
